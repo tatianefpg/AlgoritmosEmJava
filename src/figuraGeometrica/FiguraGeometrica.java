cd95b73bdcf022356e9b2a1d98920286655d8251
tatia <tatia@192.168.15.15>
2016-10-27 16:12:58 -0200

    Faça um programa em que o usuário possa digitar os lados das figuras, e como resposta obtenha a área da mesma

---
+++ src/figuraGeometrica/FiguraGeometrica.java
@@ -0,0 +1,22 @@
+
+package figuraGeometrica;
+/**
+ *
+ * @author Tatiane Paz
+ */
+public class FiguraGeometrica {
+     /*
+    *Faça um programa em que o usuário possa digitar oslados das figuras, e como
+    *resposta obtenha a área da mesma
+    */ 
+    public FiguraGeometrica(){
+    }
+    
+    public void desenha(){
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
