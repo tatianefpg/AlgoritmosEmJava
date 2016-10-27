f25268ceac9801595d71e54a3ba74a53b1fc993f
tatia <tatia@192.168.15.15>
2016-10-27 16:16:29 -0200

    Calcula a área de um triângulo:(b*h)/2

---
+++ src/figuraGeometrica/Triangulo.java
@@ -0,0 +1,16 @@
+package figuraGeometrica;
+
+/**
+ *
+ * @author Tatiane Paz
+ */
+
+public class Triangulo extends FiguraGeometrica{
+    public Triangulo(){
+    }
+    
+    @Override
+    public void desenha(){
+        System.out.println("Triangulo");
+    }
+}
