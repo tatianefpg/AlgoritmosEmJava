6ecf585a29b8401215b7e9fc5a12de7346dc0fb9
tatia <tatia@192.168.15.15>
2016-10-27 16:15:51 -0200

    Calcula a área de um quadrado: d²

---
+++ src/figuraGeometrica/Quadrado.java
@@ -0,0 +1,16 @@
+
+package figuraGeometrica;
+
+/**
+ *
+ * @author Tatiane Paz
+ */
+
+public class Quadrado extends FiguraGeometrica{
+    public Quadrado(){
+    }
+    
+    public void desenha(){
+        System.out.println("Quadrado");
+    }
+}
